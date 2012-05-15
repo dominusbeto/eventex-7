6278dede05ab5685f4f762ff4cdbf036a48a6947
Henrique Bastos <henrique@bastos.net>
2012-05-15 00:52:20 -0300

    Altera a view para renderizar o template index.html

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 from django.http import HttpResponse
-
+from django.template import loader, Context
 
 def homepage(request):
-    return HttpResponse('Bem-vindo ao EventeX!')
+    t = loader.get_template('index.html')
+    c = Context()
+
+    content = t.render(c)
+
+    return HttpResponse(content)
