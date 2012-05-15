056b22e77c6ac3f3a197095cd90ff7660684a839
Henrique Bastos <henrique@bastos.net>
2012-05-15 00:09:06 -0300

    Adiciona rota para view homepage

--- src/urls.py
+++ src/urls.py
@@ -4,13 +4,8 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'src.views.home', name='home'),
-    # url(r'^src/', include('src.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+urlpatterns = patterns('src',
+    url(r'^$', 'core.views.homepage', name='homepage'),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
