991d3790dc1235fc01c680b402c5719bf49e983f
Henrique Bastos <henrique@bastos.net>
2012-05-16 10:01:49 -0300

    Adiciona os testes para verificar unicidade do CPF e Email

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.db import IntegrityError
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
@@ -27,5 +28,28 @@ class SubscriptionModelTest(TestCase):
         )
         self.assertEquals(s.id, 1)
 
-    # 2) O cpf deve ser único
-    # 3) O email deve ser único
+
+class SubscriptionModelUniqueTest(TestCase):
+    def setUp(self):
+        "Cria uma primeira inscrição no banco."
+        Subscription.objects.create(name='Henrique Bastos', cpf='012345678901',
+                                    email='henrique@bastos.net', phone='21-96186180')
+
+    def test_cpf_must_be_unique(self):
+        "O cpf deve ser único."
+
+        # Instancia a inscrição com CPF existente
+        s = Subscription(name='Henrique Bastos', cpf='012345678901',
+                         email='outro@email.com', phone='21-96186180')
+        # Verifica se ocorre o erro de integridade ao persistir.
+        self.assertRaises(IntegrityError, s.save)
+
+    def test_email_must_be_unique(self):
+        "O email deve ser único."
+
+        # Instancia a inscrição com Email existente
+        s = Subscription(name='Henrique Bastos', cpf='00000000000',
+                         email='henrique@bastos.net', phone='21-96186180')
+        # Verifica se ocorre o erro de integridade ao persistir.
+        self.assertRaises(IntegrityError, s.save)
+
