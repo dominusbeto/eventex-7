1c89f94445b8e2bb9ea0242ae56955ee1e032694
Henrique Bastos <henrique@bastos.net>
2012-05-23 16:27:00 -0300

    Testa view success.

--- src/subscriptions/tests/__init__.py
+++ src/subscriptions/tests/__init__.py
@@ -2,3 +2,4 @@
 from .test_url import *
 from .test_models import *
 from .test_views_subscribe import *
+from .test_views_success import *
---
+++ src/subscriptions/tests/test_views_success.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from django.test import TestCase
+
+
+class SuccessViewTest(TestCase):
+    # visita a página de sucesso
+    # verifica status 200
+    # verifica template
