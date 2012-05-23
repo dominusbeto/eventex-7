c4ac0a02371545f3a3f08c4c4b15290ac1c196df
Henrique Bastos <henrique@bastos.net>
2012-05-23 05:35:19 -0300

    Planejamento dos testes do model Subscription

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -11,3 +11,9 @@ class SubscriptionUrlTest(TestCase):
     def test_get_success_page(self):
         response = self.client.get(reverse('subscriptions:success', args=[1]))
         self.assertEquals(200, response.status_code)
+
+
+class SubscriptionModelTest(TestCase):
+    # 1) O modelo deve ter os campos: name, cpf, email, phone, created_at
+    # 2) O cpf deve ser único
+    # 3) O email deve ser único
