1a5d4eba2cebd38e3f55273c03985af88a6ead72
Henrique Bastos <henrique@bastos.net>
2012-05-23 05:39:09 -0300

    Refatora testes de rota para evitar url hardcoded.

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
 class SubscriptionUrlTest(TestCase):
     def test_get_subscribe_page(self):
-        response = self.client.get('/inscricao/')
+        response = self.client.get(reverse('subscriptions:subscribe'))
         self.assertEquals(200, response.status_code)
 
     def test_get_success_page(self):
-        response = self.client.get('/inscricao/1/')
+        response = self.client.get(reverse('subscriptions:success', args=[1]))
         self.assertEquals(200, response.status_code)
