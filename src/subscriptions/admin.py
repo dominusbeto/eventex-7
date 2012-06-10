a0fb761a668487a1604305022e71ff170a77a22f
Henrique Bastos <henrique@bastos.net>
2012-06-10 19:02:20 -0300

    Adiciona campo Paid no list_display

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -6,7 +6,7 @@ from .models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'cpf', 'email', 'phone', 'created_at',
-                    'subscribed_today')
+                    'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'cpf', 'email', 'phone', 'created_at')
     list_filter = ['created_at']
