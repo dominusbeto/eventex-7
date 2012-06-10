cf8aa0d5f4aa6eeba58929d5f3e24d6c87305a74
Henrique Bastos <henrique@bastos.net>
2012-06-09 22:41:45 -0300

    Adiciona date_hierarchy.

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -5,5 +5,6 @@ from .models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'cpf', 'email', 'phone', 'created_at')
+    date_hierarchy = 'created_at'
 
 admin.site.register(Subscription, SubscriptionAdmin)
