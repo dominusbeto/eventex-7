f5f7ec66ed0d65c2d6413ed121306374bf0a6364
Henrique Bastos <henrique@bastos.net>
2012-06-17 04:59:36 -0300

    Teste do model Contact.

--- src/core/tests.py
+++ src/core/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from .models import Speaker
+from .models import Contact
 
 class HomepageTest(TestCase):
     def test_get_homepage(self):
@@ -27,6 +28,27 @@ class SpeakerModelTest(TestCase):
         self.assertEqual(u"Henrique Bastos", unicode(self.speaker))
 
 
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(name="Henrique Bastos",
+                                              slug="henrique-bastos",
+                                              url="http://henriquebastos.net",
+                                              description="Passionate software developer!",
+                                              avatar="")
+    def test_create_email(self):
+        contact = Contact.objects.create(speaker=self.speaker, kind='E', value='henrique@bastos.net')
+        self.assertEqual(1, contact.pk)
+
+    def test_create_phone(self):
+        contact = Contact.objects.create(speaker=self.speaker, kind='P', value='21-96186180')
+        self.assertEqual(1, contact.pk)
+
+    def test_create_fax(self):
+        contact = Contact.objects.create(speaker=self.speaker, kind='F', value='21-98989898')
+        self.assertEqual(1, contact.pk)
+
+
+
 class SpeakerDetailTest(TestCase):
     def setUp(self):
         Speaker.objects.create(name="Henrique Bastos",
@@ -45,4 +67,3 @@ class SpeakerDetailTest(TestCase):
     def test_speaker_in_context(self):
         speaker = self.resp.context['speaker']
         self.assertIsInstance(speaker, Speaker)
-
