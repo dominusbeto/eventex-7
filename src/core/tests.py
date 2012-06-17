5c147f7819d7ba6f64a57803bd4cd2f15830c315
Henrique Bastos <henrique@bastos.net>
2012-06-16 23:27:43 -0300

    Testa model Speaker

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from django.test import TestCase
 
+from .models import Speaker
+
 
 class HomepageTest(TestCase):
     def test_get_homepage(self):
@@ -8,3 +10,18 @@ class HomepageTest(TestCase):
         self.assertEquals(200, response.status_code)
         self.assertTemplateUsed(response, 'index.html')
 
+
+class SpeakerModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker(name="Henrique Bastos",
+                               slug="henrique-bastos",
+                               url="http://henriquebastos.net",
+                               description="Passionate software developer!",
+                               avatar="")
+        self.speaker.save()
+
+    def test_create(self):
+        self.assertEqual(1, self.speaker.pk)
+
+    def test_unicode(self):
+        self.assertEqual(u"Henrique Bastos", unicode(self.speaker))
