8452b0b34c31dda543bd9e4d7553dca997860a80
Henrique Bastos <henrique@bastos.net>
2012-06-17 07:56:27 -0300

    Rota para talk_detail

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('src.core.views',
     url(r'^palestrantes/(?P<slug>[\w-]+)/$', 'speaker_detail', name='speaker_detail'),
     url(r'^palestras/$', 'talks', name='talks'),
+    url(r'^palestras/(\d+)$', 'talk_detail', name='talk_detail'),
 )
