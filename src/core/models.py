a58388aeeb29ec8fc880053f2239a9f4e2c96421
Henrique Bastos <henrique@bastos.net>
2012-06-16 23:28:02 -0300

    Implementa model Speaker.

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,15 @@
+# coding: utf-8
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField(_('Nome'), max_length=255)
+    slug = models.SlugField(_('Slug'))
+    url = models.URLField(_('Url'))
+    description = models.TextField(_(u'Descrição'), blank=True)
+    avatar = models.FileField(_('Avatar'), upload_to='palestrantes',
+                              blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
