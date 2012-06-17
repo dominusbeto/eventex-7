c53e39c867823fb3afa2106ac5329e76b37e1904
Henrique Bastos <henrique@bastos.net>
2012-06-17 05:53:43 -0300

    View Talks

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,10 @@
 # coding: utf-8
+from datetime import time
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.generic.simple import direct_to_template
 from src.core.models import Speaker
+from src.core.models import Talk
 
 
 def homepage(request):
@@ -13,3 +15,12 @@ def homepage(request):
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
     return direct_to_template(request, 'core/speaker_detail.html', {'speaker': speaker})
+
+
+def talks(request):
+    midday = time(12)
+    context = {
+        'morning_talks': Talk.objects.filter(start_time__lt=midday),
+        'afternoon_talks': Talk.objects.filter(start_time__gte=midday),
+    }
+    return direct_to_template(request, 'core/talks.html', context)
