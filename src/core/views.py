bf6fcb301e1e63bc3276a3113408f3409c4ec017
Henrique Bastos <henrique@bastos.net>
2012-06-16 23:31:05 -0300

    Implementa view Speaker Detail

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,15 @@
 # coding: utf-8
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.views.generic.simple import direct_to_template
+from src.core.models import Speaker
 
 
 def homepage(request):
     context = RequestContext(request)
     return render_to_response('index.html', context)
+
+
+def speaker_detail(request, slug):
+    speaker = get_object_or_404(Speaker, slug=slug)
+    return direct_to_template(request, 'core/speaker_detail.html', {'speaker': speaker})
